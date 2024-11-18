bd74fab36e06a6501896cc9f45d7afca63fb477f
Jason <jtoy@grids.local>
2024-11-18 05:36:47 -0800

    updated

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ add model switcher
 add source code
 add grounding of my docs
 add strategic partner ability
+add personality
+add push to deploy
 
--- run_bot.sh
+++ run_bot.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Replace 'your_command' with the command you want to keep running
+COMMAND="ruby bot.rb"
+
+while true; do
+  echo "Starting process: $COMMAND"
+  $COMMAND
+  echo "Process terminated. Restarting in 10 seconds..."
+  sleep 10
+done
  
